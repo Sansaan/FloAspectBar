8ef069b2607512909e588e147767f5e8ebb62b56
Floraline <floraline@ophiuchus.org>
2019-09-09 01:05:02 +0200

    Oups

--- FloAspectBar.lua
+++ FloAspectBar.lua
@@ -6,10 +6,11 @@
 -- Constants
 -------------------------------------------------------------------------------
 
+local VERSION
 if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
-	local VERSION = "8.2.23"
+	VERSION = "8.2.23"
 elseif WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then
-	local VERSION = "1.13.23"
+	VERSION = "1.13.23"
 end
 
 -------------------------------------------------------------------------------
