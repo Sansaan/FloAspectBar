d55e794fc668fa6d6b769f02a75ca7ef39dfc376
Floraline <floraline@ophiuchus.org>
2019-09-09 00:34:28 +0200

    Switch version number based on classic or not

--- FloAspectBar.lua
+++ FloAspectBar.lua
@@ -6,7 +6,11 @@
 -- Constants
 -------------------------------------------------------------------------------
 
-local VERSION = "8.2.23"
+if WOW_PROJECT_ID == WOW_PROJECT_MAINLINE then
+	local VERSION = "8.2.23"
+elseif WOW_PROJECT_ID == WOW_PROJECT_CLASSIC then
+	local VERSION = "1.13.23"
+end
 
 -------------------------------------------------------------------------------
 -- Variables
